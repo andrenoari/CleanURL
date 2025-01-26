b0d3b8cde0d0adb9fb59a0b5bf0daaed8de5a0ae
Ariful Islam Khan <andrenoaricorporation@gmail.com>
2025-01-26 17:35:45 +0530

    added fbid

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Page() {
                 'utm_term',
                 'utm_content',
                 'fbclid',
+                'fbid',
                 'gclid',
                 '_ga',
                 'ref',
